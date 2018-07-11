5861b6e19eebe314e72e08b692c70a55c1248eb3
Tasha Mitchell <tamitchell14@gmail.com>
2018-07-10 20:02:35 -0400

    added Goku Attack punch sound in js

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 let attackBtn = document.getElementById('attackbtn');
 
+function soundBoard(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    document.body.appendChild(this.sound);
+    this.play = function () {
+        this.sound.play();
+    }
+}
+
 //works but for some reason works on every other click
 attackBtn.onclick = function () {
     let moveCharacter = document.getElementById('hero');
     moveCharacter.classList.toggle('hero-attacking');
+
+    let attackSound = new soundBoard("strongpunch.mp3");
+
+    attackSound.play();
+
 }
