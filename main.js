4cde5c3db8ff2b5ae8820c4db55b7e3fbea72fab
Tasha Mitchell <tamitchell14@gmail.com>
2018-07-12 11:00:37 -0400

    added battle status console

--- main.js
+++ main.js
@@ -1,52 +1,70 @@
 const attackBtn   = document.getElementById('attackbtn');
 const goku        = document.getElementById('hero');
+const enemy       = document.getElementById('enemy');
 const heroHealth  = document.getElementById('healthHero');
 const enemyHealth = document.getElementById('healthEnemy');
+const heroDamageAmt = document.getElementById('healthHero');
 const gokuAttackSound  = new Audio("strongpunch.mp3");
-
-
-//    attackEnemy(enemyReturnsAttack());
+const powerKick = new Audio("straight_punch.mp3");
 
 function disableMoveset() {
     document.querySelector('.herobtn').disabled = true;
 }
 
+function enableMoveset() {
+    document.querySelector('.herobtn').disabled = false;
+
+}
+
+function defaultGokuSprite(){
+    goku.setAttribute('src','https://piskel-imgstore-b.appspot.com/img/28151c38-85d6-11e8-9ea2-d5f6458d0832.gif');
+    goku.style.width = "220px";
+
+}
+
+function defaultEnemySprite(){
+    enemy.setAttribute('src', 'https://i.imgur.com/yGp0EuO.gif');
+    enemy.style.width = "190px";
+}
+
 //works but for some reason works on every other click
 function attack () {
+
     disableMoveset();
     //animation
     goku.classList.toggle('hero-attacking');
-
+    goku.setAttribute('src', 'https://piskel-imgstore-b.appspot.com/img/3a713d0c-85d7-11e8-935d-d5f6458d0832.gif');
+    goku.style.width = "280px"
     //sound-effect
     gokuAttackSound.play();
+
     //damage dealt to enemy
     enemyHealth.value -= 50;
 
     // enemyHealth.innerHTML.value = enemyHealth.value;
 
     enemyReturnsAttack();
+    setTimeout(defaultGokuSprite, 1000);
+
 }
 
 //enemy returns attack
 function enemyReturnsAttack() {
 
-    enemyAutoAttack();
+    setTimeout(enemyAutoAttack, 3000);
 
 
     //define enemy attacks
     function enemyAutoAttack() {
 
-        let moveCharacter = document.querySelector('#enemy');
-        moveCharacter.removeAttribute('src', 'https://i.imgur.com/yGp0EuO.gif');
+        enemy.removeAttribute('src', 'https://i.imgur.com/yGp0EuO.gif');
 
-
-        moveCharacter.setAttribute('src', 'https://piskel-imgstore-b.appspot.com/img/1271386b-853b-11e8-859f-a1429c5c6181.gif');
-
-        moveCharacter.style.width = "500px";
-
-        moveCharacter.style.transform = "scaleX(-1)"
-
-        let heroDamageAmt = document.getElementById('healthHero').value -= 150;
-
+        enemy.classList.toggle('enemy-attacking');
+        enemy.setAttribute('src', 'https://piskel-imgstore-b.appspot.com/img/dc880730-85df-11e8-b0d9-a3b55f4cb580.gif');
+        enemy.style.width = "500px";
+ powerKick.play();
+        heroDamageAmt.value -= 150;
+        enableMoveset();
+        setTimeout(defaultEnemySprite, 2000);
     }
 }
