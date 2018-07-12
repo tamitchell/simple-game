0695108b3f22fc367aff7f917f0c33125875cce8
Tasha Mitchell <tamitchell14@gmail.com>
2018-07-12 09:07:10 -0400

    cleaned up main.js file: fixed enemyReturnsAttack function

--- main.js
+++ main.js
@@ -1,43 +1,30 @@
-const attackBtn = document.getElementById('attackbtn');
-
-const heroHealth = document.getElementById('healthHero').value;
-
-let enemyHealth = document.getElementById('healthEnemy').value;
-
-function soundBoard(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    document.body.appendChild(this.sound);
-    this.play = function () {
-        this.sound.play();
-    }
-}
+const attackBtn   = document.getElementById('attackbtn');
+const goku        = document.getElementById('hero');
+const heroHealth  = document.getElementById('healthHero');
+const enemyHealth = document.getElementById('healthEnemy');
+const gokuAttackSound  = new Audio("strongpunch.mp3");
 
 
 //    attackEnemy(enemyReturnsAttack());
 
-let disableMoveset = document.querySelector('.container.controls').classList.add()
-
+function disableMoveset() {
+    document.querySelector('.herobtn').disabled = true;
+}
 
 //works but for some reason works on every other click
-let attackEnemy = attackBtn.onclick = function (event, callback) {
+function attack () {
+    disableMoveset();
     //animation
-    let moveCharacter = document.getElementById('hero');
-    moveCharacter.classList.toggle('hero-attacking');
+    goku.classList.toggle('hero-attacking');
 
     //sound-effect
-
-    let attackSound = new soundBoard("strongpunch.mp3");
-
-    attackSound.play();
-
+    gokuAttackSound.play();
     //damage dealt to enemy
+    enemyHealth.value -= 50;
 
-    let enemyDamageAmt = document.getElementById('healthEnemy').value -= 50;
+    // enemyHealth.innerHTML.value = enemyHealth.value;
 
-    enemyHealth.innerHTML.value = enemyDamageAmt;
-
-    callback();
+    enemyReturnsAttack();
 }
 
 //enemy returns attack
